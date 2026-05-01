28c30eb4720a13fc38724062930498b34113b610
Swornim Maharjan <imswornim@Swornims-MacBook-Pro.local>
2026-05-01 20:12:59 +1000

    Pass RESEND_BASE_URL to Resend client for regional endpoint

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,7 +1,9 @@
 import { Resend } from 'resend'
 
 const sendEmail = async ({ to, subject, text }) => {
-  const resend = new Resend(process.env.RESEND_API_KEY)
+  const resend = new Resend(process.env.RESEND_API_KEY, {
+    baseUrl: process.env.RESEND_BASE_URL,
+  })
   const recipients = Array.isArray(to) ? to : to.split(',').map((e) => e.trim())
   await resend.emails.send({
     from: process.env.EMAIL_FROM,
